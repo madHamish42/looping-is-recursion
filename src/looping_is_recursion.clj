31ecf07f4008ebdd8c6356cc5b436e14754c49fc
madHamish42 <pe_di@gmx.de>
2014-08-04 13:29:19 +0200

    before ex4

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -1,13 +1,26 @@
+
 (ns looping-is-recursion)
 
 (defn power [base exp]
-  ":(")
+  (let [rec (fn [val exp]
+              (if (= exp 0)
+                val
+                (recur (* val base) (dec exp))))]
+    (rec 1 exp)))
 
 (defn last-element [a-seq]
-  ":(")
+  (let [rec (fn [last rst]
+              (if (empty? rst)
+                last
+                (recur (first rst) (rest rst))))]
+    (rec nil a-seq)))
 
 (defn seq= [seq1 seq2]
-  ":(")
+  (cond
+   (and (empty? seq1) (empty? seq2)) true
+   (not= (count seq1) (count seq2)) false
+   (not= (first seq1) (first seq2)) false
+   :else (recur (rest seq1) (rest seq2))))
 
 (defn find-first-index [pred a-seq]
   ":(")
@@ -24,3 +37,4 @@
 (defn cut-at-repetition [a-seq]
   [":("])
 
+(seq= [1 2 4] '(1 2 4))
